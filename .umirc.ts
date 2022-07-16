5bd0234ee3d7ab985078d29739e3cb1e0f9106f0
Richard <1782317677@qq.com>
2022-07-16 23:34:10 +0800

    npm i @umijs/plugins @alita/plugins

--- .umirc.ts
+++ .umirc.ts
@@ -1,3 +1,12 @@
-export default {
-  npmClient: 'npm'
-};
+import { defineConfig } from "umi";
+
+export default defineConfig({
+  plugins: [
+    require.resolve("@umijs/plugins/dist/antd"),
+    require.resolve("@alita/plugins/dist/keepalive"),
+    require.resolve("@alita/plugins/dist/tabs-layout"),
+  ],
+  antd: {},
+  keepalive: [/./],
+  tabsLayout: {},
+});
